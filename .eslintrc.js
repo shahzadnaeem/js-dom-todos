2106b5df930737830c58236a4c665eb80e716976
Shahzad Naeem <shahzad65naeem@gmail.com>
2022-04-27 15:29:03 +0100

    Initial setup including Boolean favicon.ico :)

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es2021: true,
+    // node: true,
+    jasmine: true,
+    browser: true
+  },
+  extends: ['standard', 'prettier'],
+  parserOptions: {
+    ecmaVersion: 12
+  },
+  plugins: ['prettier'],
+  rules: {
+    'prettier/prettier': ['error']
+  }
+}
