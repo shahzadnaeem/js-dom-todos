5a9c65552dafed56d18fefbaf025ea7d2d3663a5
Shahzad Naeem <shahzad65naeem@gmail.com>
2022-04-28 10:36:53 +0100

    Initial implementation done

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
 const todosList = document.querySelector('#todo-list')
 const newTodoForm = document.querySelector('#todo-item-form')
+const renderCountEl = document.querySelector('#render-count')
 
-// let li = document.createElement('li')
-// li.innerText = 'Hiya!'
-// todosList.appendChild(li)
+// A counter to show how often the page has been updated since it was loaded
+// This increases every time a CRUD operation occurs
+let renderCount = 1
 
-// li = document.createElement('li')
-// li.className = 'completed'
-// li.innerText = 'See ya!'
-// todosList.appendChild(li)
+// Set up form listener to create a new TODO
 
 newTodoForm.addEventListener('submit', (ev) => {
   ev.preventDefault()
@@ -17,25 +15,100 @@ newTodoForm.addEventListener('submit', (ev) => {
 
   ev.target.reset()
 
-  console.log(`#TODO: create new todo - ${todo}`)
+  // Don't add empty TODOs
+  if (!todo) return
+
+  const postOptions = {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ title: todo, completed: false })
+  }
+
+  fetch('http://localhost:3000/todos', postOptions)
+    .then((res) => res.json())
+    .then((response) => {
+      console.log(response)
+      render()
+    })
 })
 
-// Todo list rendering ...
+// Helper functions ...
+
+function toggleCompleted(todo) {
+  const patchOptions = {
+    method: 'PATCH',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ completed: !todo.completed })
+  }
+
+  fetch(`http://localhost:3000/todos/${todo.id}`, patchOptions)
+    .then((res) => res.json())
+    .then((response) => {
+      console.log(response)
+      render() // Re-display the TODO list
+    })
+}
+
+function deleteTodo(todo) {
+  const deleteOptions = {
+    method: 'DELETE'
+  }
+
+  fetch(`http://localhost:3000/todos/${todo.id}`, deleteOptions)
+    .then((res) => res.json())
+    .then((response) => {
+      console.log(response)
+      render() // Re-display the TODO list
+    })
+}
+
+// TODO List rendering
 
 function clearTodosList() {
   todosList.innerHTML = ''
 }
 
 function createTodoListElement(todo) {
+  // We are going to create the following ...
+  // <li><span>{TODO title}</span><button>Del</button></li>
+  //
+  // Clicking the TODO will toggle its 'completed' status
+  // 'Del' button will delete it
+
   const li = document.createElement('li')
-  li.innerText = `${todo.title} (${todo.id})`
+
+  const span = document.createElement('span')
+
+  span.innerText = `${todo.title} (${todo.id})`
   if (todo.completed) {
-    li.className = 'completed'
+    span.className = 'completed'
   } else {
-    li.className = 'todo'
+    span.className = 'todo'
   }
 
+  span.addEventListener('click', (ev) => {
+    toggleCompleted(todo)
+  })
+
+  li.appendChild(span)
+
+  const deleteButton = document.createElement('button')
+  deleteButton.innerText = 'Del'
+  deleteButton.className = 'deleteTodo'
+
+  deleteButton.addEventListener('click', (ev) => {
+    ev.preventDefault()
+    if (window.confirm(`Do you want to delete this TODO? - ${todo.title}`))
+      deleteTodo(todo)
+  })
+
+  li.appendChild(deleteButton)
+
   return li
+}
+
+function setRenderCount() {
+  renderCountEl.innerText = renderCount
 }
 
 function render() {
@@ -48,6 +121,9 @@ function render() {
         todosList.appendChild(createTodoListElement(todo))
       })
     })
+
+  setRenderCount()
+  renderCount++
 }
 
 // Initialise ...
@@ -55,9 +131,9 @@ function render() {
 render()
 
 // DONE: Load initial items and populate list
-// TODO: Set up submit handler to creat a new todo
+// DONE: Set up submit handler to creat a new todo
 //         Send to server and display (re-render all?)
 //         Clear form for the next one
 
-// TODO: Add ability to mark todo as complete
-// TODO: Add ability to delete a todo
+// DONE: Add ability to mark todo as complete
+// DONE: Add ability to delete a todo
